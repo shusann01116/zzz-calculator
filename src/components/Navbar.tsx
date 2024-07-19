ce6a130b18e596e6396651d3ccbdee1d8c2d4f2d
shusann01116 <26602565+shusann01116@users.noreply.github.com>
2024-07-19 11:01:18 +0900

    wip

---
+++ src/components/Navbar.tsx
@@ -0,0 +1,24 @@
+import Link from "next/link";
+
+const links: {
+  name: string,
+  url: string,
+}[] = [
+    {
+    name: "Damage Formula (External Link)",
+    url: "https://docs.google.com/document/d/e/2PACX-1vSo82Ac3HqdI_G5_BoAqYJToK6LX4FGLPJxjPZEbhMQ-wSyFyxDFl1dr8i5czcCLJmYwxWfsXkCXN6v/pub"
+  }
+]
+
+export default function NavBar() {
+  return (
+    <div className="fixed top-0 left-0 text-center w-full py-4 border-b bg-gradient-to-b flex px-6">
+      <Link href={"/"} className="pointer-events-none">ZZZ Calculator</Link>
+      <div className="hidden ml-auto md:block">
+      {links.map(l => (
+        <Link key={l.url} href={l.url} className="text-sm text-muted-foreground hover:text-primary transition-colors">{l.name}</Link>
+      ))}
+      </div>
+    </div>
+  )
+}
---
+++ src/components/ui/myaccordion.tsx
@@ -0,0 +1,38 @@
+'use client';
+
+import { useState } from "react";
+
+export default function MyAccordion() {
+  const [activeIndex, setActiveIndex] = useState(-1)
+
+  return (
+    <div className="space-y-4">
+      <Panel title="About" isActive={activeIndex === 0} onShow={() => setActiveIndex(0)}>
+        <p>hogehogehogehoge</p>
+      </Panel>
+      <Panel title="History" isActive={activeIndex === 1} onShow={() => setActiveIndex(1)}>
+        <p>historyryryryryryry</p>
+      </Panel>
+    </div>
+  );
+}
+
+function Panel({ title, children, isActive, onShow }: {
+  title: string,
+  children: React.ReactNode,
+  isActive: boolean,
+  onShow: () => void
+}) {
+  return (
+    <div>
+      <h1 className="text-lg font-bold">{title}</h1>
+      {isActive ? (
+        <>
+          {children}
+        </>
+      ) : (
+        <button onClick={onShow} className="text-accent-foreground opacity-50">Show</button>
+      )}
+    </div>
+  );
+}
