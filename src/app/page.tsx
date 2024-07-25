35f723d71001b0ffa6b021a3fda04c2dd51f6171
shusann01116 <26602565+shusann01116@users.noreply.github.com>
2024-07-25 22:29:28 +0900

    Improve layout and move agent into component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,51 +1,14 @@
 import NavBar from "@/components/Navbar";
-import { Separator } from "@/components/ui/separator";
-import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
-import Image from "next/image";
+import Agent from "@/components/Agent";
 
 export default function Home() {
   return (
-    <main className="min-h-screen flex-col items-center bg-gradient-to-b from-zinc-700 to-neutral-900 p-4 md:p-24">
+    <main className="min-h-screen flex-col justify-center bg-gradient-to-b from-zinc-700 from-10% to-neutral-900 to-90% px-4 md:px-24">
       <div className="grid items-center justify-between">
-        <NavBar />
-        <div className="fixed bottom-0 left-0 w-full text-center">footer</div>
+        <NavBar className="fixed left-0 top-0 z-10" />
+        <div className="fixed bottom-0 left-0 h-14 w-full bg-gradient-to-t from-background/50 to-background/0 backdrop-blur-lg" />
       </div>
-      <div className="rounded-t-lg bg-black px-8 py-2 text-xl text-white">
-        Agent
-      </div>
-      <div className="items-left rounded-t-lg bg-violet-100 px-8 py-4 text-xl text-white">
-        <div className="hidden-scrollbar flex overflow-x-auto">
-          <div className="mb-2 ml-2 w-20 rounded-sm flex-none -skew-x-12 border-2 border-zinc-700 bg-violet-100">
-            <Image
-              src="/assets/img/icon/charactor/elen.png"
-              width={500}
-              height={500}
-              alt="icon of the elen"
-              className="skew-x-12 rounded-t-lg"
-            />
-          </div>
-          <div className="mb-2 ml-2 w-20 rounded-sm -skew-x-12 border-2 border-zinc-700 bg-violet-100">
-            <Image
-              src="/assets/img/icon/charactor/elen.png"
-              width={500}
-              height={500}
-              alt="icon of the elen"
-              className="skew-x-12 rounded-t-lg"
-            />
-          </div>
-        </div>
-        <div className="rounded-t-lg bg-zinc-700">
-          <div className="p-4">
-            <Image
-              src="/assets/img/charactor-art/elen.png"
-              width={500}
-              height={500}
-              alt="Picture of the elen"
-              className="logo-animation rounded-t-lg border-2 bg-violet-100"
-            />
-          </div>
-        </div>
-      </div>
+      <Agent className="m-auto mt-20 max-w-screen-lg" />
     </main>
   );
 }
---
+++ src/components/Agent.tsx
@@ -0,0 +1,47 @@
+import { cn } from "@/lib/utils";
+import { ArrowLeft } from "lucide-react";
+import Image from "next/image";
+
+export default function Agent({ className }: { className?: string }) {
+  return (
+    <div className={cn("flex flex-col rounded-t-lg bg-[#1d1f1e]", className)}>
+      <div className="flex items-center px-4 py-2 text-center text-white">
+        <ArrowLeft className="h-4 w-4" />
+        <h2 className="pl-2">エージェント詳細</h2>
+      </div>
+      <div className="items-left rounded-lg bg-violet-100 p-4 text-xl text-white">
+        <div className="hidden-scrollbar flex overflow-x-auto">
+          <div className="mb-2 ml-2 w-20 flex-none -skew-x-12 rounded-sm border-2 border-zinc-700 bg-violet-100">
+            <Image
+              src="/assets/img/icon/charactor/elen.png"
+              width={500}
+              height={500}
+              alt="icon of the elen"
+              className="skew-x-12 rounded-t-lg"
+            />
+          </div>
+          <div className="mb-2 ml-2 w-20 -skew-x-12 rounded-sm border-2 border-zinc-700 bg-violet-100">
+            <Image
+              src="/assets/img/icon/charactor/elen.png"
+              width={500}
+              height={500}
+              alt="icon of the elen"
+              className="skew-x-12 rounded-lg"
+            />
+          </div>
+        </div>
+        <div className="rounded-lg bg-zinc-700">
+          <div className="p-4">
+            <Image
+              src="/assets/img/charactor-art/elen.png"
+              width={500}
+              height={500}
+              alt="Picture of the elen"
+              className="logo-animation rounded-t-lg border-2 bg-violet-100"
+            />
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
