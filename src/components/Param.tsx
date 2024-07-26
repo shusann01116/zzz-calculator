86dd193fb1b004efdde7f21c57213e6ffa8390ec
xeno699 <xeno766@gmail.com>
2024-07-26 05:24:08 +0000

    small fix: status area design

--- src/components/Param.tsx
+++ src/components/Param.tsx
@@ -16,13 +16,13 @@ export default function Param({
   return (
     <div className={cn("flex justify-between p-1", className)}>
       <span className="text-sm text-muted-foreground">{paramName}</span>
-      <div className="grid grid-cols-1">
-        <span className="text-xs ml-2">{basicParamValue}</span>
-        <span className="text-positive text-xs">+ {correctionParamValue}</span>
+      <div className="flex justify-right">
+        <div className="grid grid-cols-1">
+          <span className="text-xs ml-2">{basicParamValue}</span>
+          <span className="text-positive text-xs">+ {correctionParamValue}</span>
+        </div>
+        <span className="text-sm ml-2">{paramValue}</span>
       </div>
-
-      <span className="text-sm">{paramValue}</span>
-      <span className="text-sm"></span>
     </div>
   );
 }
