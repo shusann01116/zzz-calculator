a1be7f2bc660c55dbaff77c2ef350258c76a36ba
xeno699 <xeno766@gmail.com>
2024-07-26 04:50:39 +0000

    change props name

--- src/components/Param.tsx
+++ src/components/Param.tsx
@@ -3,25 +3,25 @@ import { cn } from "@/lib/utils";
 export default function Param({
   className,
   paramName,
-  paramNum,
-  basicParamNum,
-  correctionParamNum,
+  paramValue,
+  basicParamValue,
+  correctionParamValue,
 }: {
   className?: string;
   paramName: string;
-  paramNum: number;
-  basicParamNum: number;
-  correctionParamNum: number;
+  paramValue: number;
+  basicParamValue: number;
+  correctionParamValue: number;
 }) {
   return (
     <div className={cn("flex justify-between p-1", className)}>
       <span className="text-sm text-muted-foreground">{paramName}</span>
       <div className="grid grid-cols-1">
-        <span className="text-xs ml-2">{basicParamNum}</span>
-        <span className="text-plus text-xs">+ {correctionParamNum}</span>
+        <span className="text-xs ml-2">{basicParamValue}</span>
+        <span className="text-plus text-xs">+ {correctionParamValue}</span>
       </div>
 
-      <span className="text-sm">{paramNum}</span>
+      <span className="text-sm">{paramValue}</span>
       <span className="text-sm"></span>
     </div>
   );
